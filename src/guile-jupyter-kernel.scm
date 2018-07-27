a7db9245a886e104138474df46c3e88b95cff629
jerry40 <epanfilov@gmail.com>
2018-07-28 02:48:42 +0300

    Return of code which contain side-effects functions like (display ..) now includes results of those side-effects

--- src/guile-jupyter-kernel.scm
+++ src/guile-jupyter-kernel.scm
@@ -68,10 +68,10 @@
 
 (define (send socket uuid header parent-header metadata content)  
     (let ((signature (get-signature notebook-info-key
-				     (string-append header
-						    parent-header
-						    metadata
-						    content))))
+				    (string-append header
+						   parent-header
+						   metadata
+						   content))))
       (zmq-send-msg-parts-bytevector socket
 				     (list uuid
 					   (string->bv DELIM)
@@ -90,36 +90,36 @@
 	  (scm->json-string `(("execution_state" . ,state)))))
 
 (define (pub-busy header- parent-header)
-  (pub header- "busy" parent-header))
+    (pub header- "busy" parent-header))
 
 (define (pub-idle header- parent-header)
-  (pub header- "idle" parent-header))
+    (pub header- "idle" parent-header))
 
 (define (heartbeat-handler)
     (zmq-message-send-bytevector socket-heartbeat
-		      (zmq-message-receive-bytevector socket-heartbeat
-					   (zmq-msg-init)))
+				 (zmq-message-receive-bytevector socket-heartbeat
+								 (zmq-msg-init)))
   (heartbeat-handler))
 
 (define (general-handler socket)
-  (let* ((parts (zmq-get-msg-parts-bytevector socket))
-	 (wire-uuid          (car parts))
-	 (wire-delimiter     (bv->string (cadr parts)))
-	 (wire-signature     (bv->string (caddr parts)))
-	 (wire-header        (json-string->scm (bv->string (list-ref parts 3))))
-	 (wire-parent-header (bv->string (list-ref parts 4)))
-	 (wire-metadata      (json-string->scm (bv->string (list-ref parts 5))))
-	 (wire-content       (json-string->scm (bv->string (list-ref parts 6)))))
-    (let ((msg-type      (hash-ref wire-header "msg_type"))
-	  (msg-username  (hash-ref wire-header "username"))
-	  (msg-session   (hash-ref wire-header "session"))
-	  (msg-version   (hash-ref wire-header "version")))
-    (let ((header- (make-header msg-username msg-session msg-version)))
-      (pub-busy header- (scm->json-string wire-header))
-      ((dispatch msg-type) socket wire-uuid header- (scm->json-string wire-header) (scm->json-string wire-metadata) wire-content)
-      (pub-idle header- (scm->json-string wire-header))
-      )))
-    (general-handler socket))
+    (let* ((parts (zmq-get-msg-parts-bytevector socket))
+	   (wire-uuid          (car parts))
+	   (wire-delimiter     (bv->string (cadr parts)))
+	   (wire-signature     (bv->string (caddr parts)))
+	   (wire-header        (json-string->scm (bv->string (list-ref parts 3))))
+	   (wire-parent-header (bv->string (list-ref parts 4)))
+	   (wire-metadata      (json-string->scm (bv->string (list-ref parts 5))))
+	   (wire-content       (json-string->scm (bv->string (list-ref parts 6)))))
+      (let ((msg-type      (hash-ref wire-header "msg_type"))
+	    (msg-username  (hash-ref wire-header "username"))
+	    (msg-session   (hash-ref wire-header "session"))
+	    (msg-version   (hash-ref wire-header "version")))
+	(let ((header- (make-header msg-username msg-session msg-version)))
+	  (pub-busy header- (scm->json-string wire-header))
+	  ((dispatch msg-type) socket wire-uuid header- (scm->json-string wire-header) (scm->json-string wire-metadata) wire-content)
+	  (pub-idle header- (scm->json-string wire-header))
+	  )))
+  (general-handler socket))
 
 ;; unknown request type, ignore it
 (define (ignore-request socket uuid header- parent-header metadata content) #f)
@@ -134,74 +134,82 @@
 	  (scm->json-string KERNEL-INFO)))
 
 (define (reply-execute-request socket uuid header- parent-header metadata content)
-  (let ((code              (string-append    "(begin " (hash-ref content "code") ")")) ;; make one s-expression from possible list
-	(silent            (hash-ref content "silent"))
-	(store-history     (hash-ref content "store_history"))
-	(user-expressions  (hash-ref content "user_expressions"))
-	(allow-stdin       (hash-ref content "allow_stdin"))
-	(stop-on-error     (hash-ref content "stop_on_error"))
-	(empty-object      (make-hash-table 1))
-	(counter           (execution-counter)))
-    (let ((err #f)
-	  (err-key #f)
-	  (evalue #f)
-	  (stacktrace #f)
-	  (result #f)
-	  (send- (lambda (socket msg-type content) (send socket uuid (header- msg-type) parent-header metadata (scm->json-string content))))
+    (let ((code              (string-append    "(begin " (hash-ref content "code") ")")) ;; make one s-expression from possible list
+	  (silent            (hash-ref content "silent"))
+	  (store-history     (hash-ref content "store_history"))
+	  (user-expressions  (hash-ref content "user_expressions"))
+	  (allow-stdin       (hash-ref content "allow_stdin"))
+	  (stop-on-error     (hash-ref content "stop_on_error"))
+	  (empty-object      (make-hash-table 1))
+	  (counter           (execution-counter)))
+      (let ((err #f)
+	    (err-key #f)
+	    (evalue #f)
+	    (stacktrace #f)
+	    (result #f)
+	    (send- (lambda (socket msg-type content) (send socket uuid (header- msg-type) parent-header metadata (scm->json-string content))))
+	    )
+	(catch #t
+	  ;; evaluate code + replace #undefined in case an expression returned nothing
+	  (lambda ()
+	    (let* ((str (with-output-to-string
+			    (lambda ()
+			      (write
+			       (eval (with-input-from-string code read) (interaction-environment))))))
+		   (tail (string-suffix-length "#<unspecified>" str)))
+	      (set! result (if (> tail 0)
+			       (string-drop-right str tail)
+			       str))))
+
+	  ;; get error message in case of an exception
+	  (lambda (key . parameters)
+	    (set! err #t) 
+	    (set! err-key (with-output-to-string (lambda () (display key))))
+	    (set! evalue (with-output-to-string (lambda () (display parameters))))
+	    (set! stacktrace (list (colorize err-key) evalue))
+	    )
+	  ;; Capture the stack here:
 	  )
-      (catch #t
-	     ;; evaluate code
-	     (lambda ()
-	       (set! result (eval-string code (interaction-environment))))
-	     ;; get error message in case of an exception
-	     (lambda (key . parameters)
-	       (set! err #t) 
-	       (set! err-key (with-output-to-string (lambda () (display key))))
-	       (set! evalue (with-output-to-string (lambda () (display parameters))))
-	       (set! stacktrace (list (colorize err-key) evalue))
-	       )
-	     ;; Capture the stack here:
-	     )
-      (send- socket-iopub "execute_input" `(("code" . ,code)
-					    ("execution_count" . ,counter)))	     
-      (when err
-	(send- socket-iopub "error"       `(("ename" . ,err-key)
-					    ("evalue" . ,evalue)
-					    ("traceback" . ,stacktrace)))
-	(send- socket      "execute_reply" `(("status" . "error")
-					    ("execution_count" . ,counter)
-					    ("ename" . ,err-key)
-					    ("evalue" . ,evalue)
-					    ("traceback" . ,stacktrace)
-					    ("payload" . [])
-					    ("user_expressions" . ,empty-object))))
-      (unless err
-	(send- socket       "execute_reply"   `(("status" . "ok")
-						("execution_count" . ,counter)
-						("payload" . [])
-						("user_expressions" . ,empty-object)))
-	(unless (unspecified? result) ;; if the result is unspecified, suppress execute_result message
-	  (send- socket-iopub "execute_result"  `(("data" .  (("text/plain" . ,(with-output-to-string (lambda () (display result))))))
-						  ("metadata" . ,empty-object)
-						  ("execution_count" . ,counter))))))))
+	(send- socket-iopub "execute_input" `(("code" . ,code)
+					      ("execution_count" . ,counter)))	     
+	(when err
+	  (send- socket-iopub "error"       `(("ename" . ,err-key)
+					      ("evalue" . ,evalue)
+					      ("traceback" . ,stacktrace)))
+	  (send- socket      "execute_reply" `(("status" . "error")
+					       ("execution_count" . ,counter)
+					       ("ename" . ,err-key)
+					       ("evalue" . ,evalue)
+					       ("traceback" . ,stacktrace)
+					       ("payload" . [])
+					       ("user_expressions" . ,empty-object))))
+	(unless err
+	  (send- socket       "execute_reply"   `(("status" . "ok")
+						  ("execution_count" . ,counter)
+						  ("payload" . [])
+						  ("user_expressions" . ,empty-object)))
+	  (unless (string-null? result)
+	    (send- socket-iopub "execute_result"  `(("data" .  (("text/plain" . ,(with-output-to-string (lambda () (display result))))))
+						    ("metadata" . ,empty-object)
+						    ("execution_count" . ,counter))))))))
 
 (define (shutdown socket uuid header- parent-header metadata content)
-  (for-each zmq-close-socket sockets)
+    (for-each zmq-close-socket sockets)
   (zmq-destroy-context context)
   (quit))
 
 (define dispatch-route
-  `(("kernel_info_request" . ,reply-kernel-info-request)
-    ("execute_request"     . ,reply-execute-request)
-    ("shutdown_request"    . ,shutdown)
-    ("comm_info_request"   . ,ignore-request)
-    ))
+    `(("kernel_info_request" . ,reply-kernel-info-request)
+      ("execute_request"     . ,reply-execute-request)
+      ("shutdown_request"    . ,shutdown)
+      ("comm_info_request"   . ,ignore-request)
+      ))
 
 (define (dispatch msg-type)
-  (let ((res (assoc-ref dispatch-route msg-type)))
-    (unless res
-      (display
-       (string-append "\n(WW) unknown message type: " msg-type "\n\n")))
-    (if res res ignore-request)))
+    (let ((res (assoc-ref dispatch-route msg-type)))
+      (unless res
+	(display
+	 (string-append "\n(WW) unknown message type: " msg-type "\n\n")))
+      (if res res ignore-request)))
 
 (general-handler socket-shell)
